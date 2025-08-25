e4c5c3863478cde26d80e2aaab20f78cd82da86c
Enrisen Tzib <enrisentzib@gmail.com>
2025-08-25 14:56:32 -0600

    Completed JSONv5

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -8,12 +8,17 @@ const version = "1.0.0"
 
 // healthcheckHandler returns the health of the system.
 func (a *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	data := map[string]string{"status": "available", "environment": a.config.env, "version": version}
-
+	data := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": a.config.env,
+			"version":     version,
+		},
+	}
 	err := a.writeJSON(w, http.StatusOK, data, nil)
-
 	if err != nil {
 		a.logger.Error(err.Error())
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
 	}
+
 }
